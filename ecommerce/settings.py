afcca71acbd6f4267316f3fb4f40451b798e5271
Ubuntu <ubuntu@ip-172-31-84-58.ec2.internal>
2019-06-25 15:30:32 +0000

    Added backends.py in the accounts app

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -109,6 +109,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.backends.EmailAuth'
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
