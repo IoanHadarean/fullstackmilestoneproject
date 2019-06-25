f6fb8460b513bfbad159a15a9066159e01363c66
Ubuntu <ubuntu@ip-172-31-84-58.ec2.internal>
2019-06-25 14:58:25 +0000

    Added form for user registration

---
+++ accounts/.~c9_invoke_UdJb6V.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+
+def register(request):
+    """Render the registration page"""
+    form = UserCreationForm()
+    args = {'form': form }
+    return render(request, 'register.html', args)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- accounts/forms.py
+++ accounts/forms.py
@@ -9,3 +9,34 @@ class UserLoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
     
+class UserRegistrationForm(UserCreationForm):
+    """ Form used to register a new user"""
+    
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Password Confirmation",
+        widget=forms.PasswordInput)
+        
+    class Meta:
+        model = User
+        fields = ['email','username', 'password1', 'password2']
+        
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(email=email).exclude(username=username):
+            raise forms.ValidationError(u'Email address must be unique')
+        return email
+        
+    def clean_password(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        
+        if not password1 or not password2:
+            raise ValidationError('Please confirm your password')
+            
+        if password1 != password2:
+            raise ValidationError('Passwords must match!')
+        return password2
