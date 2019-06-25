89ec92d73fefdb7cc6529f4fe07ce73ab94fcee1
Ubuntu <ubuntu@ip-172-31-84-58.ec2.internal>
2019-06-25 17:42:28 +0000

    Added urls and templates for password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from accounts.views import logout, login, registration, user_profile, index
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('index/', index, name='index'),
@@ -7,4 +8,14 @@ urlpatterns = [
     path('login/', login, name='login'),
     path('registration/', registration, name='registration'),
     path('profile/', user_profile, name='profile'),
+    path('password-reset/', 
+         auth_views.PasswordResetView.as_view(template_name='password_reset.html'), 
+         name='password_reset'),
+    path('password-reset/done/', 
+         auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), 
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', 
+         auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), 
+         name='password_reset_confirm'),
+         
 ]
