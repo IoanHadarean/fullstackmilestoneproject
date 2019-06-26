554daea14c651c205ad6a4d5f72421e8e5c0a527
Ubuntu <ubuntu@ip-172-31-84-58.ec2.internal>
2019-06-26 09:16:35 +0000

    Added url for password-reset-complete and template/better structure for base.html and urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,13 +8,4 @@ urlpatterns = [
     path('login/', login, name='login'),
     path('registration/', registration, name='registration'),
     path('profile/', user_profile, name='profile'),
-    path('password-reset/', 
-         auth_views.PasswordResetView.as_view(template_name='password_reset.html'), 
-         name='password_reset'),
-    path('password-reset/done/', 
-         auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), 
-         name='password_reset_done'),
-    path('password-reset-confirm/<uidb64>/<token>/', 
-         auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), 
-         name='password_reset_confirm'),
 ]
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('forum/', include('forum.urls')),
     path('accounts/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls_reset')),
 ]
