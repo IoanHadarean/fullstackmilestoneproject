5549a3abf96e9a802fb382a47b9cb6633c02fc02
Ubuntu <ubuntu@ip-172-31-84-58.ec2.internal>
2019-06-26 16:29:18 +0000

    Added checks for updating user profile/styling for profile forms

--- accounts/views.py
+++ accounts/views.py
@@ -72,13 +72,23 @@ def registration(request):
         
 def user_profile(request):
     """The user's profile page"""
-    user = User.objects.get(email=request.user.email)
-    user_form = UserUpdateForm()
-    profile_form = ProfileUpdateForm()
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, 
+                                         request.FILES, 
+                                         instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
     
-    args = {
+    
+    context = {
         'user_form': user_form,
-        'profile_form': profile_form,
-        'profile': user
+        'profile_form': profile_form
     }
-    return render(request, 'profile.html', args)
+    return render(request, 'profile.html', context)
