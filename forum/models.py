e09f1266a9d81798087513e113185faceabb441f
Ubuntu <ubuntu@ip-172-31-84-58.ec2.internal>
2019-06-30 10:40:51 +0000

    Added class for Comments

--- forum/models.py
+++ forum/models.py
@@ -15,7 +15,29 @@ class Post(models.Model):
         self.save()
         
     def approve_comments(self):
-        return self.comments.filter(approved_comments=True)
+        return self.comments.filter(approved_comment=True)
+    
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={'pk':self.pk})
     
     def __str__(self):
         return self.title
+        
+        
+class Comment(models.Model):
+    """A model class used for a single comment"""
+    post = models.ForeignKey('forum.Post', related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
+    
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+        
+    def get_absolute_url(self):
+        return reverse('post_list')
+        
+    def __str__(self):
+        return self.text
