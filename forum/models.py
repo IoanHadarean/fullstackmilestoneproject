c61c66ee6e1bfcee189e2bfda8ceeb13bd932582
Ubuntu <ubuntu@ip-172-31-84-58.ec2.internal>
2019-06-30 10:25:49 +0000

    Added class for Posts

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
 
-# Create your models here.
+class Post(models.Model):
+    """A model class that is used for a single post"""
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title =  models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True, null=True)
+    
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+        
+    def approve_comments(self):
+        return self.comments.filter(approved_comments=True)
+    
+    def __str__(self):
+        return self.title
