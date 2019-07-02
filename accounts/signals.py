55c7dd6a171ac223944ac48b6261f986f60b135d
Ubuntu <ubuntu@ip-172-31-84-58.ec2.internal>
2019-07-02 11:44:18 +0000

    Saved the user profile

--- accounts/signals.py
+++ accounts/signals.py
@@ -15,3 +15,9 @@ def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
+"""
+Save the profile for each new user.
+"""
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, created, **kwargs):
+    instance.profile.save()
