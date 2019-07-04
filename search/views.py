7cdd1405b8898898bbff2327ed31df5800f62f26
Ubuntu <ubuntu@ip-172-31-84-58.ec2.internal>
2019-07-04 11:51:12 +0000

    Added css fixes for posts and footer

--- search/views.py
+++ search/views.py
@@ -3,7 +3,11 @@ from django.db.models import Q
 from forum.models import Post
 
 def search_posts(request):
-    """Search through posts"""
+    """
+    Search posts by title, text, created_date and published_date.
+    Note: searching by author does not work because author is a
+    foreign key.
+    """
     post_list = Post.objects.filter(Q(title__icontains=request.GET['posts']) | 
                     Q(text__icontains=request.GET['posts']) |
                     Q(created_date__icontains=request.GET['posts']) | 
