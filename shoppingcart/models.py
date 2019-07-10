d149187e6dcdde0d60506c902621be65bf5e8efc
Ubuntu <ubuntu@ip-172-31-84-58.ec2.internal>
2019-07-10 10:09:57 +0000

    Added ordered and user fields for order item/fixed add to cart function

--- shoppingcart/models.py
+++ shoppingcart/models.py
@@ -46,11 +46,14 @@ class OrderItem(models.Model):
     OrderItem class that makes the connection between
     order and item
     """
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, 
+                             on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
 
     def __str__(self):
-        return self.title
+        return f"{self.quantity} of {self.item.title}"
 
 class Order(models.Model):
     """
--- shoppingcart/views.py
+++ shoppingcart/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView
+from django.utils import timezone
 from .models import Item, OrderItem, Order
 
     
@@ -28,16 +29,25 @@ class ItemDetailView(DetailView):
     
 def add_to_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
-    order_item = OrderItem.objects.create(item=item)
+    order_item, created = OrderItem.objects.get_or_create(
+        item=item,
+        user=request.user,
+        ordered=False
+    )
     order_qs = Order.objects.filter(user=request.user, ordered=False)
     if order_qs.exists():
         order = order_qs[0]
         # check if the order item is in the order
         if order.items.filter(item__slug=item.slug).exists():
             order_item.quantity += 1
+            order_item.save()
+        else:
+            order.items.add(order_item)
     else:
-        order = Order.objects.create(user=request.user)
+        ordered_date = timezone.now()
+        order = Order.objects.create(
+            user=request.user, ordered_date=ordered_date)
         order.items.add(order_item)
-    return redirect("product", kwargs={'slug': slug})
+    return redirect("product", slug=slug)
     
 
