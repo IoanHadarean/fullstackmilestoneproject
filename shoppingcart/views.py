99ce3290c54b89ab6550624408383e00ea903da9
Ubuntu <ubuntu@ip-172-31-84-58.ec2.internal>
2019-07-12 10:41:01 +0000

    Added exception handling for stripe

--- shoppingcart/views.py
+++ shoppingcart/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, View
 from django.utils import timezone
 from .forms import CheckoutForm
-from .models import Item, OrderItem, Order, BillingAddress
+from .models import Item, OrderItem, Order, BillingAddress, Payment
 
 import stripe
 stripe.api_key = settings.STRIPE_SECRET_KEY
@@ -71,13 +71,86 @@ class PaymentView(View):
         """ Get the stripe token and create a charge for a user order"""
         order = Order.objects.get(user=self.request.user, ordered=False)
         token = self.request.POST.get('stripeToken')
-        stripe.Charge.create(
-            amount=order.get_total() * 100, 
+        amount=order.get_total() * 100
+        
+        try:
+            charge = stripe.Charge.create(
+                amount=amount, 
+                currency="gbp",
+                source=token
+            )
+            
+            """Create the payment"""
+            payment = Payment()
+            payment.stripe_charge_id = charge['id']
+            payment.user = self.request.user
+            payment.amount = amount
+            payment.save()
+            
+            """Assign the payment to the order"""
+            order.ordered=True
+            order.payment = payment
+            order.save()
+            
+            messages.success(self.request, "Your order was successful.")
+            return redirect("/")
+        except stripe.error.CardError as e:
+            """Since it's a decline, stripe.error.CardError will be caught"""
+            body = e.json_body
+            err  = body.get('error', {})
+            messages.error(self.request, f"{err.get('message')}")
+            return redirect("/")
+        except stripe.error.RateLimitError as e:
+          """Too many requests made to the API too quickly"""
+          messages.error(self.request, "Rate Limit Error")
+          return redirect("/")
+        except stripe.error.InvalidRequestError as e:
+          """Invalid parameters were supplied to Stripe's API"""
+          messages.error(self.request, "Invalid parameters")
+          return redirect("/")
+        except stripe.error.AuthenticationError as e:
+          """Authentication with Stripe's API failed
+          (maybe you changed API keys recently)"""
+          messages.error(self.request, "Not authenticated")
+          return redirect("/")
+        except stripe.error.APIConnectionError as e:
+          """Network communication with Stripe failed"""
+          messages.error(self.request, "Network error")
+          return redirect("/")
+        except stripe.error.StripeError as e:
+          """Display a very generic error to the user, and maybe send
+          yourself an email"""
+          messages.error(self.request, "Something went wrong.You were not charged.Please try again.")
+          return redirect("/")
+        except Exception as e:
+          """Send an email to the user"""
+          messages.error(self.request, "A serious error occured. We have been notified.")
+          return redirect("/")
+        
+        
+        
+        
+        
+        
+        
+        
+        charge = stripe.Charge.create(
+            amount=amount, 
             currency="gbp",
             source=token
         )
-        order.ordered=True
         
+        """Create the payment"""
+        payment = Payment()
+        payment.stripe_charge_id = charge['id']
+        payment.user = self.request.user
+        payment.amount = amount
+        payment.save()
+        
+        """Assign the payment to the order"""
+        order.ordered=True
+        order.payment = payment
+        order.save()
 
 class HomeView(ListView):
     """
