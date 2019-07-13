d57970c342bd82bcc3dc914e47ae65193df8027d
Ubuntu <ubuntu@ip-172-31-84-58.ec2.internal>
2019-07-13 10:40:19 +0000

    Added being_delivered/received/refund_requested/refund_granted fields for keeping track of the order

--- shoppingcart/models.py
+++ shoppingcart/models.py
@@ -98,7 +98,22 @@ class Order(models.Model):
         'Payment', on_delete=models.SET_NULL, blank=True, null=True)
     coupon = models.ForeignKey(
         'Coupon', on_delete=models.SET_NULL, blank=True, null=True)
-
+    being_delivered = models.BooleanField(default=False)
+    received = models.BooleanField(default=False)
+    refund_requested = models.BooleanField(default=False)
+    refund_granted = models.BooleanField(default=False)
+    
+    """
+    Phases of an order:
+    1. Item added to cart
+    2. Adding a billing address
+    (Failed checkout)
+    3. Payment
+    (Preprocessing, processing, packaging etc.)
+    4. Being delivered
+    5. Received
+    6. Refunds
+    """
                                         
     def __str__(self):
         return self.user.username
