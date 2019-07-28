8d9559e4ac035fad34b0de64c4104a5318a003d0
Ubuntu <ubuntu@ip-172-31-84-58.ec2.internal>
2019-07-28 12:55:33 +0000

    Added script for adding active class for nav-filter based on href

---
+++ static/js/home.js
@@ -0,0 +1,15 @@
+/* global location */
+
+// Check the current location path
+// Get the filter nav links
+// If the href of the nav link is equal to the current
+// location path add the 'active' class to the link
+(function() {
+    var current = location.pathname;
+    var navLinks = document.getElementsByClassName('nav-filter');
+    for (var i = 0; i < navLinks.length; i++) {
+        if (navLinks[i].getAttribute('href').indexOf(current) !== -1) {
+            navLinks[i].parentElement.classList.add('active');
+        }
+    }
+})();
