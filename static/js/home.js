39f6fe08c891445107a94006867bc0fd8910e97e
Ubuntu <ubuntu@ip-172-31-84-58.ec2.internal>
2019-07-28 18:49:31 +0000

    Fixed bug with active class

--- static/js/home.js
+++ static/js/home.js
@@ -6,10 +6,14 @@
 // location path add the 'active' class to the link
 (function() {
     var current = location.pathname;
+    console.log(current);
     var navLinks = document.getElementsByClassName('nav-filter');
     for (var i = 0; i < navLinks.length; i++) {
-        if (navLinks[i].getAttribute('href').indexOf(current) !== -1) {
+        if (navLinks[i].getAttribute('href').indexOf(current) !== -1 && current != '/') {
             navLinks[i].parentElement.classList.add('active');
+        }
+        else if (current == '/') {
+            navLinks[0].parentElement.classList.add('active');
         }
     }
 })();
