4a73fb083bccf8157c34a0bd167fb13e89645a8c
Ubuntu <ubuntu@ip-172-31-84-58.ec2.internal>
2019-08-06 00:29:04 +0000

    Added customization for charts

---
+++ charts/.~c9_invoke_r1S2DM.py
@@ -0,0 +1,238 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from datetime import date
+import datetime
+from shoppingcart.models import Order
+from django.views.generic import View
+
+orders = Order.objects.filter(ordered=True)
+today = date.today()
+
+
+class ChartsView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'charts/charts.html', {})
+        
+        
+def day_orders():
+    last_3_days = []
+    for i in range(0, 3):
+        last_3_days.append(today - datetime.timedelta(i))
+    
+    daily_orders = []
+    orders_last_3_days_total = 0
+    sales_last_3_days_total = 0
+    for order in orders:
+        if order.ordered_date.date() in last_3_days:
+            orders_last_3_days_total += 1
+            sales_last_3_days_total += order.get_total()
+            for date_time in last_3_days:
+                orders_per_day = 0
+                if order.ordered_date.date() == date_time:
+                    orders_per_day += 1
+                    daily_orders.append({date_time.strftime("%A") : orders_per_day})
+                else:
+                    daily_orders.append({date_time.strftime("%A") : 0})
+
+                    
+    return daily_orders, orders_last_3_days_total, sales_last_3_days_total
+    
+
+def month_orders():
+    last_3_months = []
+    last_3_months_datetime = []
+    for i in range(0, 3):
+        last_3_months.append((today - datetime.timedelta(i * 365/12)).month)
+        last_3_months_datetime.append(today - datetime.timedelta(i * 365/12))
+        
+        
+    monthly_orders = []
+    orders_last_3_months_total = 0
+    sales_last_3_months_total = 0
+    for order in orders:
+        if order.ordered_date.date().month in last_3_months:
+            orders_last_3_months_total += 1
+            sales_last_3_months_total += order.get_total()
+            for date_time in last_3_months_datetime:
+                orders_per_month = 0
+                if order.ordered_date.date().month == date_time.month:
+                    orders_per_month += 1
+                    monthly_orders.append({date_time.strftime("%B"): orders_per_month})
+                else:
+                    monthly_orders.append({date_time.strftime("%B"): orders_per_month})
+                    
+    return monthly_orders, orders_last_3_months_total, sales_last_3_months_total
+    
+
+def last_3_days_orders():
+    
+    daily_orders, orders_last_3_days_total, sales_last_3_days_total = day_orders()
+
+    days = []
+    for order in daily_orders:
+        if list(order.keys())[0] not in days:
+            days.append(list(order.keys())[0])
+            
+    first_day = {}
+    second_day = {}
+    third_day = {}
+    orders_first_day = 0
+    orders_second_day = 0
+    orders_third_day = 0
+    for order in daily_orders:
+        if days[0] in list(order.keys())[0]:
+            orders_first_day += order[days[0]]
+            first_day.update({days[0]: orders_first_day})
+        elif days[1] in list(order.keys())[0]:
+            orders_second_day += order[days[1]]
+            second_day.update({days[1]: orders_second_day})
+        elif days[2] in list(order.keys())[0]:
+            orders_third_day += order[days[2]]
+            third_day.update({days[2]: orders_third_day})
+            
+    orders_last_3_days = []
+    orders_last_3_days.append(first_day)
+    orders_last_3_days.append(second_day)
+    orders_last_3_days.append(third_day)
+    orders_last_3_days.append({'Total': orders_last_3_days_total})
+    
+    return orders_last_3_days
+
+
+def last_3_months_orders():
+    
+    monthly_orders, orders_last_3_months_total, sales_last_3_months_total = month_orders()
+    
+    months = []
+    for order in monthly_orders:
+        if list(order.keys())[0] not in months:
+            months.append(list(order.keys())[0])
+            
+    first_month = {}
+    second_month = {}
+    third_month = {}
+    orders_first_month = 0
+    orders_second_month = 0
+    orders_third_month = 0
+    
+    for order in monthly_orders:
+        if months[0] in list(order.keys())[0]:
+            orders_first_month += order[months[0]]
+            first_month.update({months[0]: orders_first_month})
+        elif months[1] in list(order.keys())[0]:
+            orders_second_month += order[months[1]]
+            second_month.update({months[1]: orders_second_month})
+        elif months[2] in list(order.keys())[0]:
+            orders_third_month += order[months[2]]
+            third_month.update({months[2]: orders_third_month})
+            
+    orders_last_3_months = []
+    orders_last_3_months.append(first_month)
+    orders_last_3_months.append(second_month)
+    orders_last_3_months.append(third_month)
+    orders_last_3_months.append({'Total': orders_last_3_months_total})
+    
+    return orders_last_3_months
+        
+        
+def last_3_days_sales():
+
+    daily_orders, orders_last_3_days_total, sales_last_3_days_total = day_orders()
+
+    days = []
+    for order in daily_orders:
+        if list(order.keys())[0] not in days:
+            days.append(list(order.keys())[0])
+            
+    first_day = {}
+    second_day = {}
+    third_day = {}
+    sales_first_day = 0
+    sales_second_day = 0
+    sales_third_day = 0
+    for order in orders:
+        if days[0] == order.ordered_date.date().strftime("%A"):
+            sales_first_day += order.get_total()
+            first_day.update({days[0]: sales_first_day})
+        elif days[1] == order.ordered_date.date().strftime("%A"):
+            sales_second_day += order.get_total()
+            second_day.update({days[1]: sales_second_day})
+        elif days[2] == order.ordered_date.date().strftime("%A"):
+            sales_third_day += order.get_total()
+            third_day.update({days[2]: sales_third_day})
+            
+    if first_day == {}:
+        first_day.update({days[0]: 0})
+    if second_day == {}:
+        second_day.update({days[1]: 0})
+    if third_day == {}:
+        third_day.update({days[2]: 0})
+            
+    sales_last_3_days = []
+    sales_last_3_days.append(first_day)
+    sales_last_3_days.append(second_day)
+    sales_last_3_days.append(third_day)
+    sales_last_3_days.append({'Total': sales_last_3_days_total})
+    
+    return sales_last_3_days
+        
+        
+def last_3_months_sales():
+    
+    monthly_orders, orders_last_3_months_total, sales_last_3_months_total = month_orders()
+    
+    months = []
+    for order in monthly_orders:
+        if list(order.keys())[0] not in months:
+            months.append(list(order.keys())[0])
+            
+    first_month = {}
+    second_month = {}
+    third_month = {}
+    sales_first_month = 0
+    sales_second_month = 0
+    sales_third_month = 0
+    
+    for order in orders:
+        if months[0] == order.ordered_date.date().strftime("%B"):
+            sales_first_month += order.get_total()
+            first_month.update({months[0]: str(sales_first_month) + '£'})
+        elif months[1] == order.ordered_date.date().strftime("%B"):
+            sales_second_month += order.get_total()
+            second_month.update({months[1]: str(sales_second_month) + '£'})
+        elif months[2] == order.ordered_date.date().strftime("%B"):
+            sales_third_month += order.get_total()
+            third_month.update({months[2]: sales_third_month + '£'})
+            
+    if first_month == {}:
+        first_month.update({months[0]: 0})
+    if second_month == {}:
+        second_month.update({months[1]: 0})
+    if third_month == {}:
+        third_month.update({months[2]: 0})
+            
+    sales_last_3_months = []
+    sales_last_3_months.append(first_month)
+    sales_last_3_months.append(second_month)
+    sales_last_3_months.append(third_month)
+    sales_last_3_months.append({'Total': sales_last_3_months_total})
+    
+    return sales_last_3_months
+    
+    
+def get_data(request, *args, **kwargs):
+    
+    orders_last_3_days = last_3_days_orders()
+    orders_last_3_months = last_3_months_orders()
+    sales_last_3_days = last_3_days_sales()
+    sales_last_3_months = last_3_months_sales()
+    
+    data = {
+        'orders_last_3_days': orders_last_3_days,
+        'orders_last_3_months': orders_last_3_months,
+        'sales_last_3_days': sales_last_3_days,
+        'sales_last_3_months': sales_last_3_months,
+    }
+    
+    return JsonResponse(data)
+        
--- ecommerce/static/js/custom/charts.js
+++ ecommerce/static/js/custom/charts.js
@@ -22,74 +22,91 @@ getChartsData();
 
 // Construct the charts with the data received from the AJAX request
 function constructCharts(data) {
-    
+
     // Iterate through the data and get the keys and values
     Object.keys(data).forEach(function(key) {
-            
-            var dataLabels = [];
-            var dataValues = [];
-            for (var i = 0; i < data[key].length; i++) {
-                console.log(data[key][i]);
-                var dataLabel = Object.keys(data[key][i]).toString();
-                var dataValue = Object.values(data[key][i]).toString();
-                dataLabels.push(dataLabel);
-                dataValues.push(dataValue);
-            }
-            
-            console.log(dataLabels);
-        
-            // Split the text at "_"
-            let keySplit = key.split("_");
-            
-            // Capitalize the first word
-            let capitalizedFirstWord = keySplit[0][0].toUpperCase() + keySplit[0].slice(1);
-            
-            // Get the remaining of the text section
-            let textSection = '';
-            for (var i = 1; i < keySplit.length; i++) {
-              textSection += keySplit[i] + ' ';
-            }
-            
-            // Put together the capitalized text
-            let capitalizedText = capitalizedFirstWord + ' ' + textSection;
-            
-            var ctx = document.getElementById(key).getContext('2d');
-            var chart = new Chart(ctx, {
-                type: 'line',
-                data: {
-                    labels: dataLabels,
-                    datasets: [{
-                        label: capitalizedText,
-                        data: dataValues,
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
+
+        var dataLabels = [];
+        var dataValues = [];
+        for (var i = 0; i < data[key].length; i++) {
+            var dataLabel = Object.keys(data[key][i]).toString();
+            var dataValue = Object.values(data[key][i]).toString();
+            dataLabels.push(dataLabel);
+            dataValues.push(dataValue);
+        }
+
+
+        // Split the text at "_"
+        let keySplit = key.split("_");
+
+        // Capitalize the first word
+        let capitalizedFirstWord = keySplit[0][0].toUpperCase() + keySplit[0].slice(1);
+
+        // Get the remaining of the text section
+        let textSection = '';
+        for (var i = 1; i < keySplit.length; i++) {
+            textSection += keySplit[i] + ' ';
+        }
+
+        // Put together the capitalized text
+        let capitalizedText = capitalizedFirstWord + ' ' + textSection;
+
+        var ctx = document.getElementById(key).getContext('2d');
+        var chart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: dataLabels,
+                datasets: [{
+                    label: capitalizedText,
+                    data: dataValues,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            min: 0,
+                            beginAtZero: true,
+                            callback: function(value, index, values) {
+                                if (Math.floor(value) === value) {
+                                    return value;
+                                }
                             }
-                        }]
+                        }
+                    }],
+                },
+                tooltips: {
+                    callbacks: {
+                        label: function(tooltipItem, data) {
+                            var value = data.datasets[0].data[tooltipItem.index];
+                            var label = data.labels[tooltipItem.index];
+
+                            if (value === 0.1) {
+                                value = 0;
+                            }
+
+                            return label + ': ' + value;
+                        }
                     }
                 }
-            });
-        }
-    );
+            }
+        });
+    });
 }
